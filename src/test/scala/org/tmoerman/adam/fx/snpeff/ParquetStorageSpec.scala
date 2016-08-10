0ab0d1a66a4292a3d1090ecf8a41dcf13cda247b
Thomas Moerman <thomas.moerman@gmail.com>
2016-08-10 11:39:14 +0200

    tests for save as VCF

--- src/test/scala/org/tmoerman/adam/fx/snpeff/ParquetStorageSpec.scala
+++ src/test/scala/org/tmoerman/adam/fx/snpeff/ParquetStorageSpec.scala
@@ -8,11 +8,13 @@ import org.tmoerman.adam.fx.BaseSparkContextSpec
 import scala.reflect.io.File
 
 /**
- * @author Thomas Moerman
- */
+  * @author Thomas Moerman
+  */
 class ParquetStorageSpec extends BaseSparkContextSpec with BeforeAndAfter {
 
   val vcf     = "src/test/resources/small.snpEff.vcf"
+
+  val out_vcf = "src/test/resources/small.snpEff.out.vcf"
 
   val temp    = "src/test/temp/small.snpEff.adam"
 
@@ -36,16 +38,35 @@ class ParquetStorageSpec extends BaseSparkContextSpec with BeforeAndAfter {
     assert(fromFile.take(10) === fromParquet.take(10))
   }
 
-  def deleteParquetFile() {
+  // TODO save as vcf
+
+  "Saving AnnotatedGenotypes loaded from vcf back to vcf" should "work" in {
+    val fromVCF = sc.loadAnnotatedGenotypes(vcf)
+
+    fromVCF.map(_.getGenotype).toVariantContext().saveAsVcf(out_vcf, coalesceTo = Some(1))
+  }
+
+  "Saving AnnotatedGenotypes loaded from Parquet back to vcf" should "work" in {
+    val fromVCF = sc.loadAnnotatedGenotypes(vcf)
+
+    fromVCF.adamParquetSave(temp)
+
+    val fromParquet = sc.loadAnnotatedGenotypes(temp)
+
+    fromParquet.map(_.getGenotype).toVariantContext().saveAsVcf(out_vcf, coalesceTo = Some(1))
+  }
+
+  def deleteFiles() {
     File(temp).deleteRecursively()
+    File(out_vcf).deleteRecursively()
   }
 
   before {
-    deleteParquetFile()
+    deleteFiles()
   }
 
   after {
-    deleteParquetFile()
+    deleteFiles()
   }
 
 }
